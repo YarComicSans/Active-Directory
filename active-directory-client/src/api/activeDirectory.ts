08db56f58b0858f3adac2ca2b9720afb3b0048ee
YarComicSans <danaleks98@gmail.com>
2021-04-02 18:56:53 +0300

    Fixing: * fetch issues with no-cors * forgotten resolve of promise's value

--- active-directory-client/src/api/activeDirectory.ts
+++ active-directory-client/src/api/activeDirectory.ts
@@ -1,3 +1,5 @@
+import { get } from './common'
+
 export type RequestStatus = 'success' | 'failed' | 'running'
 
 export type RequestInfoDto = {
@@ -24,76 +26,40 @@ export type UserFetchParams = {
     pageNumber: number
 }
 
-// export const getUsers = (userParams: UserFetchParams) : UserInfosDto => {
-//     // return get<UserInfosDto>('/users')
-//     return {
-//         users: [
-//             {
-//                 dn: 'CN=Guest,CN=Users,DC=mydomain,DC=ru',
-//                 sAMAccountName: 'Guest',
-//                 whenCreated: '20210207140220.0Z',
-//                 pwdLastSet: '0',
-//                 userAccountControl: '66082',
-//                 cn: 'Guest',
-//                 description: 'Built-in account for guest access to the computer/domain'
-//             },
-//             {
-//                 dn: 'CN=Administrator,CN=Users,DC=mydomain,DC=ru',
-//                 sAMAccountName: 'Administrator',
-//                 whenCreated: '20210207140220.0Z',
-//                 pwdLastSet: '132571801407584760',
-//                 userAccountControl: '512',
-//                 cn: 'Administrator',
-//                 description: 'Built-in account for administering the computer/domain'
-//             },
-//             {
-//                 dn: 'CN=krbtgt,CN=Users,DC=mydomain,DC=ru',
-//                 sAMAccountName: 'krbtgt',
-//                 whenCreated: '20210207140220.0Z',
-//                 pwdLastSet: '132571801407942610',
-//                 userAccountControl: '514',
-//                 cn: 'krbtgt',
-//                 description: 'Key Distribution Center Service Account'
-//             }
-//          ],
-//         totalCount: 3
-//     }
-// }
-
-export const getUsers = (userParams: UserFetchParams) : Promise<UserInfosDto> => {
-    // return get<UserInfosDto>('/users')
-    return new Promise((resolve) => {
-        return resolve({
-            users: [
-                {
-                    dn: 'CN=Guest,CN=Users,DC=mydomain,DC=ru',
-                    sAMAccountName: 'Guest',
-                    whenCreated: '20210207140220.0Z',
-                    pwdLastSet: '0',
-                    userAccountControl: '66082',
-                    cn: 'Guest',
-                    description: 'Built-in account for guest access to the computer/domain'
-                },
-                {
-                    dn: 'CN=Administrator,CN=Users,DC=mydomain,DC=ru',
-                    sAMAccountName: 'Administrator',
-                    whenCreated: '20210207140220.0Z',
-                    pwdLastSet: '132571801407584760',
-                    userAccountControl: '512',
-                    cn: 'Administrator',
-                    description: 'Built-in account for administering the computer/domain'
-                },
-                {
-                    dn: 'CN=krbtgt,CN=Users,DC=mydomain,DC=ru',
-                    sAMAccountName: 'krbtgt',
-                    whenCreated: '20210207140220.0Z',
-                    pwdLastSet: '132571801407942610',
-                    userAccountControl: '514',
-                    cn: 'krbtgt',
-                    description: 'Key Distribution Center Service Account'
-                }
-            ],
-            totalCount: 3
-        })
-    })
+export const getUsers = (userParams: UserFetchParams) => {
+    return get<UserInfosDto>('active-directory/users')
+    // return new Promise((resolve) => {
+    //     return resolve({
+    //         users: [
+    //             {
+    //                 dn: 'CN=Guest,CN=Users,DC=mydomain,DC=ru',
+    //                 sAMAccountName: 'Guest',
+    //                 whenCreated: '20210207140220.0Z',
+    //                 pwdLastSet: '0',
+    //                 userAccountControl: '66082',
+    //                 cn: 'Guest',
+    //                 description: 'Built-in account for guest access to the computer/domain'
+    //             },
+    //             {
+    //                 dn: 'CN=Administrator,CN=Users,DC=mydomain,DC=ru',
+    //                 sAMAccountName: 'Administrator',
+    //                 whenCreated: '20210207140220.0Z',
+    //                 pwdLastSet: '132571801407584760',
+    //                 userAccountControl: '512',
+    //                 cn: 'Administrator',
+    //                 description: 'Built-in account for administering the computer/domain'
+    //             },
+    //             {
+    //                 dn: 'CN=krbtgt,CN=Users,DC=mydomain,DC=ru',
+    //                 sAMAccountName: 'krbtgt',
+    //                 whenCreated: '20210207140220.0Z',
+    //                 pwdLastSet: '132571801407942610',
+    //                 userAccountControl: '514',
+    //                 cn: 'krbtgt',
+    //                 description: 'Key Distribution Center Service Account'
+    //             }
+    //         ],
+    //         totalCount: 3
+    //     })
+    // })
 }
--- active-directory-client/src/api/common.ts
+++ active-directory-client/src/api/common.ts
@@ -1,17 +1,18 @@
-import fetch from 'cross-fetch'
+import fetch from 'isomorphic-fetch'
 
-const url = 'http://localhost:3005/'
+const url = 'http://localhost:3002/'
 
 export async function get<T> (resource: string): Promise<T> {
     const params = {
-        mode: 'no-cors',
+        // mode: 'no-cors',
         method: 'GET',
         headers: { Accept: 'application/json' }
     } as const
 
     const response = await fetch(`${url}${resource}`, params)
 
-    return (await response.json()) as T
+    const result = (await response.json()) as T
+    return result
 }
 
 export async function put (resource: string) {
@@ -19,5 +20,5 @@ export async function put (resource: string) {
         mode: 'no-cors',
         method: 'PUT'
     } as const
-    await fetch(`${url}${resource}`, params)
+    await fetch(`${url}/${resource}`, params)
 }
